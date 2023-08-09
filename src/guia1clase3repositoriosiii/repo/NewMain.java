40e668763edfd798ccc82975032f4dcfecef802e
Lorena Capuano <lorenacapuano@hotmail.com>
2023-08-09 11:35:30 -0300

    hola otra vez

--- src/guia1clase3repositoriosiii/repo/NewMain.java
+++ src/guia1clase3repositoriosiii/repo/NewMain.java
@@ -17,6 +17,7 @@ public class NewMain {
     public static void main(String[] args) {
         System.out.println("Hola grupo");
         System.out.println("Hola Joaco");
+        System.out.println("Hola otra vez");
     }
     
 }
