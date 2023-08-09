3f0789a668ca3189d9cfede5b37846de1bad51d5
Diegomar2004 <141736663+Diegomar2004@users.noreply.github.com>
2023-08-09 11:13:33 -0300

    agrego sout
    holaa

--- src/guia1clase3repositoriosiii/Guia1Clase3Repositoriosiii.java
+++ src/guia1clase3repositoriosiii/Guia1Clase3Repositoriosiii.java
@@ -16,6 +16,9 @@ public class Guia1Clase3Repositoriosiii {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("hola soy Diego...!!!");
+        
     }
     
 }
